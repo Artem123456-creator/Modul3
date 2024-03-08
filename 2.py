b3d9828d32d60b144b7cae7d28fbfe2116a0665e
Artem123456-creator <Arti7713@gmail.com>
2024-03-08 23:01:09 +0200

    second comit

--- 2.py
+++ 2.py
@@ -7,4 +7,4 @@ def get_numbers_ticket(min, max, quantity):
         result.add(random_number)
     return sorted(list(result))  
 lottery_numbers = get_numbers_ticket(1, 49, 6)
-print("Ваші лотерейні числа:", lottery_numbers)
+print("Your lottery numbers:", lottery_numbers)
