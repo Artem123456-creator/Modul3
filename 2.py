c877ca2bc579194841bba9c2daf9dbfbddced168
Artem123456-creator <Arti7713@gmail.com>
2024-03-11 22:13:41 +0200

    fixed bug with quantity

--- 2.py
+++ 2.py
@@ -1,8 +1,8 @@
 from random import randint
 
 def get_numbers_ticket(min, max, quantity):
-    result =set()
-    if min >= 1 and max <= 1000:
+    result = set()
+    if min >= 1 and max <= 1000 and max - min + 1 >= quantity:
         while len(result) < quantity:
             random_number = randint(min, max)
             result.add(random_number)
@@ -12,6 +12,8 @@ def get_numbers_ticket(min, max, quantity):
 lottery_numbers = get_numbers_ticket(1, 49, 6)
 lottery_numbers2 = get_numbers_ticket(1, 1001, 6)
 lottery_numbers3 = get_numbers_ticket(-1, 49, 6)
+lottery_numbers4 = get_numbers_ticket(10, 15, 8)
 print("Your lottery numbers:", lottery_numbers)
 print("Your lottery numbers:", lottery_numbers2)
 print("Your lottery numbers:", lottery_numbers3)
+print("Your lottery numbers:", lottery_numbers4)
